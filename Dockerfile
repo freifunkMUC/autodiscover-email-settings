6424681db6199ef1bccbe8790120684d4f001818
GoliathLabs <git@xdfr.de>
2025-12-19 15:45:47 +0100

    feat: update koa, deps & workflows

--- Dockerfile
+++ Dockerfile
@@ -14,32 +14,3 @@ RUN set -ex; \
 COPY views /app/views
 
 CMD ["node", "/app/index"]
-
-# Arguments to label built container
-ARG VCS_REF=unknown
-ARG BUILD_DATE=unknown
-ARG VERSION=1.4.0
-
-# Container labels (http://label-schema.org/)
-# Container annotations (https://github.com/opencontainers/image-spec)
-LABEL maintainer="Monogramm Maintainers <opensource at monogramm dot io>" \
-      product="Autodiscover Email Settings" \
-      version=$VERSION \
-      org.label-schema.vcs-ref=$VCS_REF \
-      org.label-schema.vcs-url="https://github.com/Monogramm/autodiscover-email-settings" \
-      org.label-schema.build-date=$BUILD_DATE \
-      org.label-schema.name="Autodiscover Email Settings" \
-      org.label-schema.description="Provides Autodiscover capabilities for IMAP/POP/SMTP/LDAP services on Microsoft Outlook/Apple Mail and Autoconfig capabilities for Thunderbird" \
-      org.label-schema.url="https://github.com/Monogramm/autodiscover-email-settings" \
-      org.label-schema.vendor="Monogramm" \
-      org.label-schema.version=$VERSION \
-      org.label-schema.schema-version="1.0" \
-      org.opencontainers.image.revision=$VCS_REF \
-      org.opencontainers.image.source="https://github.com/Monogramm/autodiscover-email-settings" \
-      org.opencontainers.image.created=$BUILD_DATE \
-      org.opencontainers.image.title="Autodiscover Email Settings" \
-      org.opencontainers.image.description="Provides Autodiscover capabilities for IMAP/POP/SMTP/LDAP services on Microsoft Outlook/Apple Mail and Autoconfig capabilities for Thunderbird" \
-      org.opencontainers.image.url="https://github.com/Monogramm/autodiscover-email-settings" \
-      org.opencontainers.image.vendor="Monogramm" \
-      org.opencontainers.image.version=$VERSION \
-      org.opencontainers.image.authors="Monogramm Maintainers <opensource at monogramm dot io>"
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 "use strict";
 
-const path		= require("path");
-const app		= require("koa")();
-const swig		= require("koa-swig");
-const body		= require("koa-buddy");
-const router	= require("koa-router")();
-const settings	= require("./settings.js");
+const path = require("path");
+const Koa = require("koa");
+const app = new Koa();
+const views = require("@ladjs/koa-views");
+const xmlBody = require("koa-xml-body");
+const bodyParser = require("koa-bodyparser");
+const Router = require("@koa/router");
+const router = new Router();
+const settings = require("./settings.js");
 
 function findChild(name, children, def = null) {
 	for (let child of children) {
@@ -16,50 +19,62 @@ function findChild(name, children, def = null) {
 	return def;
 }
 
+function extractEmailFromXml(raw) {
+	if (!raw) return null;
+	const m = raw.match(/<EMailAddress>([^<]+)<\/EMailAddress>/i);
+	return m ? m[1] : null;
+}
+
 // Microsoft Outlook / Apple Mail
-function *autodiscover() {
-	this.set("Content-Type", "application/xml");
+async function autodiscover(ctx) {
+	ctx.set("Content-Type", "application/xml");
 
-	const request	= this.request.body && this.request.body.root ? 
-		findChild("Request", this.request.body.root.children) : 
-		null;
-	const schema	= request !== null ? 
-		findChild("AcceptableResponseSchema", request.children) : 
-		null;
-	const xmlns		= schema !== null ? 
-		schema.content : 
-		"http://schemas.microsoft.com/exchange/autodiscover/responseschema/2006";
+	// Try to use parsed body if available, otherwise fallback to raw XML extraction
+	let email = null;
+	if (ctx.request.body && typeof ctx.request.body === 'object') {
+		const request = ctx.request.body.root && ctx.request.body.root.children ?
+			findChild("Request", ctx.request.body.root.children) : null;
+		const schema = request !== null ? findChild("AcceptableResponseSchema", request.children) : null;
+		const xmlns = schema !== null ? schema.content : "http://schemas.microsoft.com/exchange/autodiscover/responseschema/2006";
 
-	let email		= request !== null ? 
-		findChild("EMailAddress", request.children) : 
-		null;
+		let emailNode = request !== null ? findChild("EMailAddress", request.children) : null;
+		if (emailNode && emailNode.content) {
+			email = emailNode.content;
+		}
+
+		ctx.state._xmlns = xmlns;
+	}
+
+	if (!email) {
+		const raw = ctx.request.rawBody || (typeof ctx.request.body === 'string' ? ctx.request.body : null);
+		email = extractEmailFromXml(raw);
+	}
 
 	let username;
 	let domain;
-	if ( email === null || email.content === null ) {
-		email		= "";
-		username	= "";
-		domain		= settings.domain;
-	} else if ( ~email.content.indexOf("@") ) {
-		email		= email.content;
-		username	= email.split("@")[0];
-		domain		= email.split("@")[1];
+	if (!email) {
+		email = "";
+		username = "";
+		domain = settings.domain;
+	} else if (email.indexOf("@") !== -1) {
+		username = email.split("@")[0];
+		domain = email.split("@")[1];
 	} else {
-		username	= email.content;
-		domain		= settings.domain;
-		email		= username + "@" + domain;
+		username = email;
+		domain = settings.domain;
+		email = `${username}@${domain}`;
 	}
 
-	const imapenc	= settings.imap.socket === "STARTTLS" ? "TLS" : settings.imap.socket;
-	const popenc	= settings.pop.socket === "STARTTLS" ? "TLS" : settings.pop.socket;
-	const smtpenc	= settings.smtp.socket === "STARTTLS" ? "TLS" : settings.smtp.socket;
+	const imapenc = settings.imap.socket === "STARTTLS" ? "TLS" : settings.imap.socket;
+	const popenc = settings.pop.socket === "STARTTLS" ? "TLS" : settings.pop.socket;
+	const smtpenc = settings.smtp.socket === "STARTTLS" ? "TLS" : settings.smtp.socket;
 
-	const imapssl	= settings.imap.socket === "SSL" ? "on" : "off";
-	const popssl	= settings.pop.socket === "SSL" ? "on" : "off";
-	const smtpssl	= settings.smtp.socket === "SSL" ? "on" : "off";
+	const imapssl = settings.imap.socket === "SSL" ? "on" : "off";
+	const popssl = settings.pop.socket === "SSL" ? "on" : "off";
+	const smtpssl = settings.smtp.socket === "SSL" ? "on" : "off";
 
-	yield this.render("autodiscover", {
-		schema: xmlns,
+	await ctx.render('autodiscover.xml', {
+		schema: ctx.state._xmlns || "http://schemas.microsoft.com/exchange/autodiscover/responseschema/2006",
 		email,
 		username,
 		domain,
@@ -79,44 +94,43 @@ router.post("/Autodiscover/Autodiscover.xml", autodiscover);
 
 
 // Thunderbird
-router.get("/mail/config-v1.1.xml", function *autoconfig() {
-	this.set("Content-Type", "application/xml");
-	yield this.render("autoconfig");
+router.get("/mail/config-v1.1.xml", async (ctx) => {
+	ctx.set("Content-Type", "application/xml");
+	await ctx.render('autoconfig.xml');
 });
 
 
 // iOS / Apple Mail (/email.mobileconfig?email=username@domain.com or /email.mobileconfig?email=username)
-router.get("/email.mobileconfig", function *autoconfig() {
-	let email = this.request.query.email;
+router.get("/email.mobileconfig", async (ctx) => {
+	let email = ctx.request.query.email;
 
 	if (!email) {
-		this.status = 400;
-
+		ctx.status = 400;
 		return;
 	}
 
 	let username;
 	let domain;
-	if ( ~email.indexOf("@") ) {
-		username	= email.split("@")[0];
-		domain		= email.split("@")[1];
+	if (email.indexOf("@") !== -1) {
+		username = email.split("@")[0];
+		domain = email.split("@")[1];
 	} else {
-		username	= email;
-		domain		= settings.domain;
-		email		= username + "@" + domain;
+		username = email;
+		domain = settings.domain;
+		email = `${username}@${domain}`;
 	}
 
-	const filename	= `${domain}.mobileconfig`;
+	const filename = `${domain}.mobileconfig`;
 
-	const imapssl	= settings.imap.socket === "SSL" || settings.imap.socket === "STARTTLS" ? "true" : "false";
-	const popssl	= settings.pop.socket === "SSL" || settings.pop.socket === "STARTTLS" ? "true" : "false";
-	const smtpssl	= settings.smtp.socket === "SSL" || settings.smtp.socket === "STARTTLS" ? "true" : "false";
-	const ldapssl	= settings.ldap.socket === "SSL" || settings.ldap.port === "636" ? "true" : "false";
+	const imapssl = settings.imap.socket === "SSL" || settings.imap.socket === "STARTTLS" ? "true" : "false";
+	const popssl = settings.pop.socket === "SSL" || settings.pop.socket === "STARTTLS" ? "true" : "false";
+	const smtpssl = settings.smtp.socket === "SSL" || settings.smtp.socket === "STARTTLS" ? "true" : "false";
+	const ldapssl = settings.ldap.socket === "SSL" || settings.ldap.port === "636" ? "true" : "false";
 
-	this.set("Content-Type", "application/x-apple-aspen-config; charset=utf-8");
-	this.set("Content-Disposition", `attachment; filename="${filename}"`);
+	ctx.set("Content-Type", "application/x-apple-aspen-config; charset=utf-8");
+	ctx.set("Content-Disposition", `attachment; filename="${filename}"`);
 
-	yield this.render("mobileconfig", {
+	await ctx.render('mobileconfig.xml', {
 		email,
 		username,
 		domain,
@@ -129,35 +143,36 @@ router.get("/email.mobileconfig", function *autoconfig() {
 
 
 // Generic support page
-router.get("/", function *index() {
-	yield this.render("index.html");
+router.get("/", async (ctx) => {
+	await ctx.render('index.html');
 });
 
-router.get("/favicon.ico", function *icon() {
-	yield this.render("favicon.ico");
+router.get("/favicon.ico", async (ctx) => {
+	await ctx.render('favicon.ico');
 });
 
-app.context.render = swig({
-	root: path.join(__dirname, "views"),
-	autoescape: true,
-	cache: "memory",
-	ext: "xml",
-	locals: settings
-});
+app.use(views(path.join(__dirname, 'views'), {
+	map: { xml: 'nunjucks' }
+}));
 
-app.use(function *fixContentType(next) {
-	let type = this.request.headers["content-type"];
-
-	if (type && type.indexOf("text/xml") === 0) {
-		let newType = type.replace("text/xml", "application/xml");
-
-		this.request.headers["content-type"] = newType;
+app.use(async (ctx, next) => {
+	// Normalize text/xml to application/xml for downstream parsers
+	const type = ctx.request.headers['content-type'];
+	if (type && type.indexOf('text/xml') === 0) {
+		ctx.request.headers['content-type'] = type.replace('text/xml', 'application/xml');
 	}
-
-	yield next;
+	await next();
 });
 
-app.use(body());
+// parse XML bodies into ctx.request.body and keep raw body on ctx.request.rawBody
+app.use(xmlBody({
+	enableRaw: true,
+	xmlOptions: { explicitArray: false, explicitChildren: true, preserveChildrenOrder: true, charsAsChildren: false }
+}));
+
+// parse urlencoded/json bodies
+app.use(bodyParser());
+
 app.use(router.routes());
 app.use(router.allowedMethods());
 
